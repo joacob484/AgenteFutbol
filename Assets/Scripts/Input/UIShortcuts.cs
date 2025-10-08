14de63feda1c92b62a355055f9f54afde82030f2
joacob484 <jbertaux@correo.um.edu.uy>
2025-10-08 10:29:58 -0300

    algo

--- Assets/Scripts/Input/UIShortcuts.cs
+++ Assets/Scripts/Input/UIShortcuts.cs
@@ -5,13 +5,13 @@ namespace AF.Inputs
 {
     public class UIShortcuts : MonoBehaviour
     {
-        public void GoMainMenu()   => UIRouter.Go("MainMenu");
-        public void GoDashboard()  => UIRouter.Go("Dashboard");
-        public void GoTalents()    => UIRouter.Go("Talents");
-        public void GoPlayers()    => UIRouter.Go("Players");
-        public void GoClubs()      => UIRouter.Go("Clubs");
-        public void GoNews()       => UIRouter.Go("News");
-        public void GoFinance()    => UIRouter.Go("Finance");
-        public void GoSettings()   => UIRouter.Go("Settings");
+        public void GoMainMenu()  => UIRouter.Instance?.Show("MainMenu");
+        public void GoDashboard() => UIRouter.Instance?.Show("Dashboard");
+        public void GoTalents()   => UIRouter.Instance?.Show("Talents");
+        public void GoPlayers()   => UIRouter.Instance?.Show("Players");
+        public void GoClubs()     => UIRouter.Instance?.Show("Clubs");
+        public void GoNews()      => UIRouter.Instance?.Show("News");
+        public void GoFinance()   => UIRouter.Instance?.Show("Finance");
+        public void GoSettings()  => UIRouter.Instance?.Show("Settings");
     }
 }
